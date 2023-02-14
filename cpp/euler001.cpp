4e5b01776662633e27ecad1ef5f5841f6a219521
sabarbanel <sm412949@dal.ca>
2023-02-14 11:32:47 -0400

    removed python template code

--- cpp/euler001.cpp
+++ cpp/euler001.cpp
@@ -1,15 +1,3 @@
-/*
-multiples = [] # Will contain all multiples of 3 and 5
-
-numberCap = 1000 # The highest number that the program will check for multiples
-
-for i in range(1, numberCap):
-    if i % 3 == 0 or i % 5 == 0:
-        multiples.append(i)
-
-print sum(multiples)
-*/
-
 #include <iostream>
 
 int main() {
@@ -25,3 +13,4 @@ int main() {
 
     return 0;
 }
+
