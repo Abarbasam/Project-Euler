9c37322d6fddd7c8a0f92bcb71583eafa936973a
sabarbanel <sm412949@dal.ca>
2023-02-14 12:37:01 -0400

    Had some ideas for code optimization. Got ChatGPT to help me. Runtime has reduced from 0.021s to 0.017s. Pretty good!

--- cpp/euler003.cpp
+++ cpp/euler003.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 
 bool is_prime(int num) {
+    // Low prime check. If number does not fall within the set
+    // defined by f(k) = 6k += 1, kEN, it is not prime
     if (num <= 1) return false;
+    if (num == 2 || num == 3) return true;
+    if (num % 6 != 1 && num % 6 != 5) return false;
 
-    for (int i = 2; i < sqrt(num); i++) {
+    for (int i = 5; i < sqrt(num); i++) {
         if (num % i == 0) return false;
     }
     return true;
@@ -15,12 +19,16 @@ bool is_prime(int num) {
 
 int main() {
     long num = 600851475143;
-    int largest_factor = 0;
+    int largest_factor = -1;
 
-    for (int i = 2; i <= sqrt(num); i++) {
+    // The problem is only concerned about finding the largest prime factor.
+    // Start searching for prime factors from sqrt(num) going down. 
+    for (int i = floor(sqrt(num)); i >= 2; i--) {
         if (num % i == 0) {
-            if (is_prime(i)) largest_factor = i;
-            if (is_prime(num/i)) largest_factor = num/i;
+            if (is_prime(i)) {
+                largest_factor = i;
+                break;
+            }
         }
     }
     
