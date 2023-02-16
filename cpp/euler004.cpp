415bd67811473aa1efa78a374408cbaa0693b5f0
sabarbanel <sm412949@dal.ca>
2023-02-16 12:58:45 -0400

    for this sample size, the new method was far faster. 0.037second.

--- cpp/euler004.cpp
+++ cpp/euler004.cpp
@@ -19,20 +19,33 @@ bool isPalindrome(int num) {
     return false;
 }
 
-// Exhaustive search of all combinations 
+// Starting at the largest product of two three-digit numbers, then working my
+// way down finding all the palindromes and only testing that those are divisible
+// by two less than four digit numbers.
 int main() {
     int number = 999;
-    int largestPalindrome = 0;
-    int mult = 0;
+    int largestPalindromePossible = number * number;
+    int largestPalindrome = largestPalindromePossible;
+    bool largestPalindromeFound = false;
 
-    for (int i = number; i > 0; i--) {
-        for (int j = i; j > 0; j--) {
-            mult = i * j;
-            if ( isPalindrome(mult) ) {
-                if (largestPalindrome < mult) largestPalindrome = mult; 
+    while ( !largestPalindromeFound ) {
+        // I worry that excessive string conversions will slowdown the runtime.
+        if ( isPalindrome(largestPalindrome) ) {
+            // Check if it is divisible by a number between 1 and 999
+            for (int i = number; i > 2; i--) {
+                if ( largestPalindrome % i == 0 ) {
+                    if ( largestPalindrome / i < 1000) {
+                        largestPalindromeFound = true;
+                        break;
+                    }
+                }
             }
         }
+
+        if (!largestPalindromeFound) { largestPalindrome--; }
     }
 
     cout << largestPalindrome << endl;
+    exit(0);
+
 }
